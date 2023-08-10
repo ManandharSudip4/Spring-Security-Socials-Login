186dc82012a2de1254dc2f99ae0346c610da6b65
ManandharSudip4 <manandharsudip8@gmail.com>
2023-08-10 12:34:46 +0545

    Basic Single Sign On With GitHub

---
+++ src/main/java/com/mstech/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mstech.springsecurityfacebooklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityFacebookLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityFacebookLoginApplication.class, args);
+	}
+
+}
